540760833e75f01208449b0f77c45dc09502814c
AUraihQA <AUraih@qa.com>
2021-02-24 15:02:52 +0000

    New structure

--- src/App.js
+++ src/App.js
@@ -1,34 +1,43 @@
 'use strict';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './Components/Components/09-Routing/Home';
-import About from './Components/Components/09-Routing/About';
-import Product from './Components/Components/09-Routing/Product';
-import User from "./Components/Components/09-Routing/User";
-import Nav from "./Components/Components/09-Routing/Nav";
+import Home from './Components/09-Routing/Home';
+import About from './Components/09-Routing/About';
+import Product from './Components/09-Routing/Product';
+import User from "./Components/09-Routing/User";
+import Nav from ".Components/09-Routing/Nav";
+import Notfound from './Components/09-Routing/NotFound';
+import Tesco from './Components/10-DataRequests/Tesco';
 
 const App = () => {
   return (
     <div className="App">
-       <div className="App">
-      <Router>
-        <Nav />
-        <Switch>
-          <Route path="/" exact>
-            <Home />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/shop">
-            <Product />
-          </Route>
-          <Route path="/user/:id">
-            <User />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
-      
+      <div className="App">
+        <Router>
+          <Nav />
+          <Switch>
+            <Route path="/" exact>
+              <Home />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/shop">
+              <Product />
+            </Route>
+            <Route path="/user/:id">
+              <User />
+            </Route>
+
+            <Route>
+              <Tesco path="/tesco" />
+            </Route>
+            <Route>
+              <Notfound />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+
     </div>
   );
 }
